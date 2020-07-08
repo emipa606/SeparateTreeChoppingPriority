a46e811594eea2f826a158c6bce7aeb542f0bfe1
emipa606 <emil.offermo@litet.net>
2020-07-08 21:00:37 +0200

    First publish

---
+++ Source/TreesOnly/WorkGiver_PlantsCut_NoTrees.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace TreesOnly
+{
+    public class WorkGiver_PlantsCut_NoTrees : WorkGiver_PlantsCut
+    {
+        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+        {
+            var allDesignations = pawn.Map.designationManager.allDesignations;
+
+            for (int i = 0; i < allDesignations.Count; i++)
+            {
+                var designation = allDesignations[i];
+
+                if (designation.def == DesignationDefOf.CutPlant || designation.def == DesignationDefOf.HarvestPlant)
+                {
+                    var plant = designation.target.Thing as Plant;
+                    if (!plant.def.plant.IsTree)
+                    {
+                        yield return designation.target.Thing;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/TreesOnly/WorkGiver_TreesChop.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace TreesOnly
+{
+    public class WorkGiver_TreesChop : WorkGiver_Scanner
+    {
+        public override PathEndMode PathEndMode
+        {
+            get
+            {
+                return PathEndMode.Touch;
+            }
+        }
+
+        public override Danger MaxPathDanger(Pawn pawn)
+        {
+            return Danger.Deadly;
+        }
+
+        public override IEnumerable<Thing> PotentialWorkThingsGlobal(Pawn pawn)
+        {
+            List<Designation> desList = pawn.Map.designationManager.allDesignations;
+
+            for (int i = 0; i < desList.Count; i++)
+            {
+                Designation des = desList[i];             
+                if (des.def == DesignationDefOf.CutPlant || des.def == DesignationDefOf.HarvestPlant)
+                {
+                    Plant plant = des.target.Thing as Plant;
+
+                    if (plant != null)
+                    {
+                        if (plant.def.plant.IsTree)
+                        {
+                            yield return des.target.Thing;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+        {
+            if (t.def.category != ThingCategory.Plant)
+            {
+                return null;
+            }
+            if (!pawn.CanReserve(t, 1, -1, null, false))
+            {
+                return null;
+            }
+            if (t.IsForbidden(pawn))
+            {
+                return null;
+            }
+            if (t.IsBurning())
+            {
+                return null;
+            }
+            Job result = null;
+            foreach (Designation current in pawn.Map.designationManager.AllDesignationsOn(t))
+            {                
+                if (current.def == DesignationDefOf.HarvestPlant)
+                {
+                    if (!((Plant)t).HarvestableNow || !((Plant)t).def.plant.IsTree)
+                    {
+                        break;
+                    }
+                    result = new Job(JobDefOf.HarvestDesignated, t);
+                    break;
+                }
+                else if (current.def == DesignationDefOf.CutPlant)
+                {
+                    if (!((Plant)t).def.plant.IsTree)
+                    {
+                        break;
+                    }
+                    result = new Job(JobDefOf.CutPlantDesignated , t);
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
