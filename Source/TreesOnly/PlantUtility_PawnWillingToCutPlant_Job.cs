38ed951e658b7774be6444347dfe011a4a111694
Luboš Luňák <l.lunak@centrum.cz>
2021-08-08 15:53:29 +0200

    block tree cutting only for trees
    In my previous commit I accidentally blocked all plant cutting,
    not just trees.

--- Source/TreesOnly/PlantUtility_PawnWillingToCutPlant_Job.cs
+++ Source/TreesOnly/PlantUtility_PawnWillingToCutPlant_Job.cs
@@ -7,8 +7,11 @@ namespace TreesOnly
     [HarmonyPatch(typeof(PlantUtility), "PawnWillingToCutPlant_Job")]
     public static class PlantUtility_PawnWillingToCutPlant_Job
     {
-        public static bool Prefix(ref bool __result, Pawn pawn)
+        public static bool Prefix(ref bool __result, Thing plant, Pawn pawn)
         {
+            if (!plant.def.plant.IsTree)
+                return true; // execute the original version of the function
+
             // Possibly block cutting down trees when considered for clearing place for other work.
             if (pawn.workSettings.WorkIsActive(WorkTypeDefOf.TreeChopping))
             {
